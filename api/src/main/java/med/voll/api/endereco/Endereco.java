28f64ef4f223d2270d36c0398634e08db460a1a3
DonatoJoao <joaod.morais@gmail.com>
2023-02-07 08:40:26 -0300

    Aplicando padrão DTO

---
+++ api/src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,5 @@
+package med.voll.api.endereco;
+
+public record Endereco(String logradouro, String bairro, String cep, String cidade, String uf, String complemento, String numero) {
+
+}
---
+++ api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -0,0 +1,7 @@
+package med.voll.api.medico;
+
+import med.voll.api.endereco.Endereco;
+
+public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, Endereco endereco) {
+
+}
